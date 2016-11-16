84ea61ff050bc423708bc28a7615f3cf78c63241
francocas <franquicas@hotmail.com>
2016-11-15 23:01:11 -0300

    Add files via upload

---
+++ RECUPERATORIOS/Casartelli.Franco.2D.TP3/gimnasioTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesInstanciables;
+namespace gimnasioTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidarNulo()
+        {
+            Gimnasio hola = new Gimnasio();
+            Assert.IsNotNull(hola.Alumno, null);
+        }
+    }
+}
