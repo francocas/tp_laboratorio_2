032db288bcbd07e87278a9a19a23665d7986065f
francocas <franquicas@hotmail.com>
2016-11-15 23:00:02 -0300

    Add files via upload

---
+++ RECUPERATORIOS/Casartelli.Franco.2D.TP3/EntidadesInstanciables/Alumno.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesAbstractas;
+
+namespace EntidadesInstanciables
+{
+    [Serializable]
+    public sealed class Alumno : PersonaGimnasio
+    {
+        public enum EEstadoCuenta{Deudor, AlDia, MesPrueba};
+
+        #region ATRIBUTOS
+        private Gimnasio.EClases _ClaseQueToma;
+
+        public Gimnasio.EClases ClaseQueToma
+        {
+            get { return _ClaseQueToma; }
+        }
+
+        private EEstadoCuenta _estadoCuenta;
+
+        public EEstadoCuenta EstadoCuenta
+        {
+            get { return _estadoCuenta; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORES
+        public Alumno()
+        {
+
+        }
+
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, Gimnasio.EClases claseQueToma)
+            : base(id : id, nombre : nombre, apellido : apellido, dni : dni, nacionalidad : nacionalidad)
+        {
+            this._ClaseQueToma = claseQueToma;
+        }
+
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, Gimnasio.EClases claseQueToma, EEstadoCuenta estadoCuenta)
+            : this(id: id, nombre: nombre, apellido: apellido, dni: dni, nacionalidad: nacionalidad, claseQueToma: claseQueToma)
+        {
+            this._estadoCuenta = estadoCuenta;
+        }
+        #endregion
+
+        #region SOBRECARGAS
+        protected override string ParticiparEnClase()
+        {
+            return "TOMA CLASES DE " + this._ClaseQueToma.ToString();
+        }
+
+        protected override string MostrarDatos()
+        {
+            return base.MostrarDatos() + this.ParticiparEnClase();
+        }
+
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+
+        public static bool operator ==(Alumno a, Gimnasio.EClases clase)
+        {
+            bool retorno = false;
+            if (a._estadoCuenta != EEstadoCuenta.Deudor && a._ClaseQueToma == clase)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Alumno a, Gimnasio.EClases clase)
+        {
+            return !(a == clase);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        #endregion
+    }
+}
---
+++ RECUPERATORIOS/Casartelli.Franco.2D.TP3/EntidadesInstanciables/Instructor.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesAbstractas;
+namespace EntidadesInstanciables
+{
+    [Serializable]
+
+    public class Instructor : PersonaGimnasio
+    {
+        #region ATRIBUTOS
+        private Queue<Gimnasio.EClases> _clasesDelDia;
+        private static Random _random;
+
+        #endregion
+
+        #region CONSTRUCTORES
+        static Instructor()
+        {
+            _random = new Random();
+        }
+        public Instructor()
+        {
+
+        }
+        public Instructor(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+            : base(id: id, nombre: nombre, apellido: apellido, dni: dni, nacionalidad: nacionalidad)
+        {
+            this._clasesDelDia = new Queue<Gimnasio.EClases>();
+            this._randomClases();
+        }
+
+        private void _randomClases()
+        {
+            int r;
+            for (int i = 0; i < 2; i++)
+            {
+                r = _random.Next(1, 5);
+                switch (r)
+                {
+                    case 1:
+                        this._clasesDelDia.Enqueue(Gimnasio.EClases.CrossFit);
+                        break;
+                    case 2:
+                        this._clasesDelDia.Enqueue(Gimnasio.EClases.Natacion);
+                        break;
+                    case 3:
+                        this._clasesDelDia.Enqueue(Gimnasio.EClases.Pilates);
+                            break;
+                    case 4:
+                        this._clasesDelDia.Enqueue(Gimnasio.EClases.Yoga);
+                        break;
+                }
+            }
+        }
+        #endregion
+
+        #region SOBRECARGAS
+        protected override string ParticiparEnClase()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("CLASES DEL DIA ");
+            foreach (Gimnasio.EClases a in this._clasesDelDia)
+            {
+                sb.AppendLine(a.ToString());
+            }
+            return sb.ToString();
+        }
+
+        protected override string MostrarDatos()
+        {
+            
+            return base.MostrarDatos() + ParticiparEnClase();
+        }
+
+        public override string ToString()
+        {
+            return MostrarDatos();
+        }
+
+        public static bool operator ==(Instructor i, Gimnasio.EClases clases)
+        {
+            bool retorno = false;
+            foreach (Gimnasio.EClases a in i._clasesDelDia)
+            {
+                if (a == clases)
+                {
+                    retorno = true;
+                    break;
+                }
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Instructor i, Gimnasio.EClases clases)
+        {
+            return !(i == clases);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+#endregion
+    }
+}
